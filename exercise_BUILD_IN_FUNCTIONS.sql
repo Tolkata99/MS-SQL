73f22b0a9dc373aac562fdd3f5d16a234fc3bec6
Anatoli Mihaylov <69379704+Tolkata99@users.noreply.github.com>
2022-05-21 13:46:44 +0300

    Update exercise_BUILD_IN_FUNCTIONS.sql
    Add tasks

--- exercise_BUILD_IN_FUNCTIONS.sql
+++ exercise_BUILD_IN_FUNCTIONS.sql
@@ -104,7 +104,7 @@ ORDER BY [ISO Code]
 
 
 --13
-  SELECT [PeakName],[RiverName],LOWER(CONCAT([PeakName], SUBSTRING([RiverName],2,LEN(RiverName)))) 
+  SELECT p.[PeakName],r.[RiverName],LOWER(CONCAT([PeakName], SUBSTRING([RiverName],2,LEN(RiverName)))) 
                 AS [Mix]
     FROM [Peaks]  AS p,
          [Rivers] AS r
@@ -113,5 +113,38 @@ ORDER BY [Mix]
 
 USE [Diablo]
 
-SELECT [Name], DATEPART(YEAR,MONTH,DAY,[Start]) FROM [Games]
+--14
+SELECT TOP(50) [Name], FORMAT([Start],'yyyy-MM-dd') AS [Start]
+FROM [Games]
+WHERE DATEPART(YEAR,[Start]) IN (2011, 2012)
+ORDER BY [Start], [Name]
 
+--15 
+SELECT [Username], SUBSTRING(Email,CHARINDEX('@',Email,1) + 1,LEN(Email)) AS [Email Provider] 
+FROM [Users]
+ORDER BY [Email] ,[Username] 
+
+
+--16
+
+SELECT [Username], [IpAddress] AS [IP Address]
+FROM [Users]
+WHERE [IpAddress] LIKE '___.1_%._%.___'
+
+--17
+
+SELECT [Name] AS [Game],
+       CASE
+	       WHEN DATEPART(HOUR, [Start]) >= 0 AND DATEPART(HOUR, [Start]) < 12 THEN 'Morning'
+		   WHEN DATEPART(HOUR, [Start]) >= 12 AND DATEPART(HOUR, [Start]) < 18 THEN 'Afternoon'
+		   ELSE 'Evening'
+		   END AS [Part of the Day]
+		   ,
+      CASE
+	      WHEN [Duration] <= 3 THEN 'Extra Short'
+		  WHEN [Duration] BETWEEN 4 AND 6 THEN 'Short'
+		  WHEN [Duration] > 6 THEN 'Long'
+		  ELSE 'Extra Long'
+		  END AS [Duration]
+FROM [Games]
+ORDER BY [Game], [Duration]
