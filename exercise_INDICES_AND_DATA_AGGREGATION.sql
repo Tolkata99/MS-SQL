73907684a3230227d3ec2d2aaa6f9d84ac5ef042
Anatoli Mihaylov <anatoligithub@gmail.com>
2022-05-31 18:22:38 +0300

    Update exercise_INDICES_AND_DATA_AGGREGATION.sql
    add tasks

--- exercise_INDICES_AND_DATA_AGGREGATION.sql
+++ exercise_INDICES_AND_DATA_AGGREGATION.sql
@@ -71,6 +71,27 @@ SELECT [AgeGroup]
 
 
 
+  --10
+  SELECT DISTINCT LEFT([FirstName],1) AS [FirstLetter]
+    FROM [WizzardDeposits]
+   WHERE [DepositGroup] = 'Troll Chest'
+ORDER BY [FirstLetter]
 
 
+--11
+  SELECT [DepositGroup]
+         ,[IsDepositExpired]
+         ,AVG(DepositInterest) AS [AverageInterest]
+    FROM [WizzardDeposits]
+   WHERE [DepositStartDate] > '1985-01-01'
+GROUP BY [DepositGroup], [IsDepositExpired]
+ORDER BY [DepositGroup] DESC, [IsDepositExpired] ASC
 
+--12
+ SELECT  SUM([Difference]) AS [SumDifference] FROM (SELECT [FirstName] AS [Host Wizard]
+        ,[DepositAmount] AS [Host Wizard Deposit]
+		,LEAD([FirstName]) OVER(ORDER BY [ID]) AS [Guest Wizard]
+		,LEAD([DepositAmount]) OVER(ORDER BY [ID]) AS [Guest Wizard deposit]
+		,[DepositAmount] - LEAD([DepositAmount]) OVER(ORDER BY [ID]) as [Difference] 
+    FROM [WizzardDeposits]) AS [QUERI]
+	
