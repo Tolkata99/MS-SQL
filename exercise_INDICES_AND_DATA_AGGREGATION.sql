e1c04b0536121336c92b993d490799d4dc0abe97
Anatoli Mihaylov <anatoligithub@gmail.com>
2022-05-31 09:07:57 +0300

    Update exercise_INDICES_AND_DATA_AGGREGATION.sql
    Add tasks

--- exercise_INDICES_AND_DATA_AGGREGATION.sql
+++ exercise_INDICES_AND_DATA_AGGREGATION.sql
@@ -2,15 +2,50 @@ USE [Gringotts]
 
 --1
  SELECT COUNT(*) AS [Count] 
- FROM [WizzardDeposits]
+   FROM [WizzardDeposits]
 
 --2
  SELECT MAX(MagicWandSize) AS [LongestMagicWand]
- FROM [WizzardDeposits]       
+   FROM [WizzardDeposits]       
 
 --3
-  SELECT
-          [DepositGroup]
+   SELECT   [DepositGroup]
 	      ,MAX([MagicWandSize]) AS [LongestMagicWand]
      FROM [WizzardDeposits]
  GROUP BY [DepositGroup]
+
+ --4
+
+     SELECT TOP (2) [DepositGroup]              
+		   FROM [WizzardDeposits]
+       GROUP BY [DepositGroup]
+       ORDER BY AVG([MagicWandSize])
+
+--5
+  SELECT [DepositGroup]
+         ,SUM([DepositAmount]) AS [TotalSum]
+    FROM [WizzardDeposits]
+GROUP BY [DepositGroup]
+
+--6
+  SELECT [DepositGroup]
+         ,SUM([DepositAmount]) AS [TotalSum]
+    FROM [WizzardDeposits]
+   WHERE [MagicWandCreator] = 'Ollivander family'
+GROUP BY [DepositGroup]
+
+--7
+   SELECT [DepositGroup]
+          ,SUM([DepositAmount]) AS [TotalSum] 
+     FROM [WizzardDeposits]
+	WHERE [MagicWandCreator] = 'Ollivander family'
+ GROUP BY [DepositGroup]
+   HAVING SUM([DepositAmount]) < 150000
+ ORDER BY [TotalSum] DESC
+
+
+
+
+
+
+
