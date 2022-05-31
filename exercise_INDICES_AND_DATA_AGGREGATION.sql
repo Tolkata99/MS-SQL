e24e01f03e20c88937776b2a3a9c9665a3f04184
Anatoli Mihaylov <anatoligithub@gmail.com>
2022-05-31 15:09:49 +0300

    Update exercise_INDICES_AND_DATA_AGGREGATION.sql
    add tasks

--- exercise_INDICES_AND_DATA_AGGREGATION.sql
+++ exercise_INDICES_AND_DATA_AGGREGATION.sql
@@ -43,6 +43,31 @@ GROUP BY [DepositGroup]
    HAVING SUM([DepositAmount]) < 150000
  ORDER BY [TotalSum] DESC
 
+--8
+  SELECT [DepositGroup]
+         ,[MagicWandCreator]
+		 ,MIN([DepositCharge]) AS [MinDepositCharge]
+    FROM [WizzardDeposits] 
+GROUP BY [DepositGroup] ,[MagicWandCreator]
+ORDER BY [MagicWandCreator], [DepositGroup]
+
+--9
+SELECT [AgeGroup]  
+       ,COUNT(ID)
+       FROM (
+	           SELECT *,
+                 CASE  
+         		    WHEN [Age] BETWEEN 0 AND 10 THEN '[0-10]'
+         			WHEN [Age] BETWEEN 11 AND 20 THEN '[11-20]'
+         			WHEN [Age] BETWEEN 21 AND 30 THEN '[21-30]'
+         			WHEN [Age] BETWEEN 31 AND 40 THEN '[31-40]'
+         			WHEN [Age] BETWEEN 41 AND 50 THEN '[41-50]'
+         			WHEN [Age] BETWEEN 51 AND 60 THEN '[51-60]'
+         			WHEN [Age] >= 61 THEN '[61+]' 
+                 END AS [AgeGroup]
+            FROM [WizzardDeposits]
+		    ) AS [GroupingQuery]
+  GROUP BY [AgeGroup]
 
 
 
