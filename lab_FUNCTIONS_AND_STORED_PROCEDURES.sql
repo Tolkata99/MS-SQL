408d0c7a0d472e59e51d8797d0112a92c25ab3bc
Anatoli Mihaylov <anatoligithub@gmail.com>
2022-06-04 16:01:09 +0300

    Update lab_FUNCTIONS_AND_STORED_PROCEDURES.sql
    Add Table-Valued functions

--- lab_FUNCTIONS_AND_STORED_PROCEDURES.sql
+++ lab_FUNCTIONS_AND_STORED_PROCEDURES.sql
@@ -12,3 +12,12 @@ BEGIN
 	RETURN @projectWeeks
 END
 --Table-Valued Function
+CREATE FUNCTION udf_AverageSalaryByDepartment()
+RETURNS TABLE AS
+RETURN
+(
+    SELECT d.[Name] AS [Department], AVG(e.[Salary] AS [AverageSalary]
+	  FROM [Departments] AS d
+	  JOIN [Employees] AS e ON d.[DepartmentID] = e.[DepartmentID]
+	  GROUP BY d.[DepartmentID],d.[Name]
+)
